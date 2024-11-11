f4f8d18af7ab804ec5b4c6e886e8926fc61cea65
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-11 12:37:24 +0700

    feat: initialize a Discord bot application (#6)
    * feat: modify bin script to login Discord bot client
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * chore: remove unused sample functions
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/bin.ts
+++ src/bin.ts
@@ -1,23 +1,15 @@
 #!/usr/bin/env node
 
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
-import { fibonacciSequence } from "./sequence.js";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
-yargs(hideBin(process.argv))
-  .scriptName("my_fibonacci")
-  .version("0.0.0")
-  .command(
-    "$0 <n>",
-    "Generate a Fibonacci sequence up to the given number of terms.",
-    (yargs) =>
-      yargs.positional("n", {
-        demandOption: true,
-        describe: "The number of terms",
-        type: "number",
-      }),
-    (argv) => {
-      process.stdout.write(fibonacciSequence(argv.n).join(" ") + "\n");
-    },
-  )
-  .parse();
+try {
+  const readyClient = await new Promise<Client<true>>((resolve, reject) => {
+    const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+    client.once(Events.ClientReady, resolve);
+    client.login().catch(reject);
+  });
+  console.info("Logged in as:", readyClient.user.tag);
+} catch (err) {
+  console.error("Failed to login:", err);
+  process.exitCode = 1;
+}
