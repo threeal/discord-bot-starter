edb59779e5eb6c38b614f4d8b4da6628e5a56b2c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-12 21:35:46 +0700

    feat: specify base user directory in client options (#18)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/bin.ts
+++ src/bin.ts
@@ -5,6 +5,7 @@ import "@sapphire/plugin-logger/register";
 import { GatewayIntentBits } from "discord.js";
 
 const client = new SapphireClient({
+  baseUserDirectory: import.meta.dirname,
   intents: [GatewayIntentBits.Guilds],
   loadMessageCommandListeners: true,
 });
