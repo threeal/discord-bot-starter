fd3a6976d33ca7e15a2809da631e2ad484151e3f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-14 17:28:42 +0700

    refactor: specify generic arguments for sample listeners (#42)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,16 +1,12 @@
 import { container, Listener } from "@sapphire/framework";
-import type { Client } from "discord.js";
+import { ClientEvents, Events } from "discord.js";
 
-export class ReadyListener extends Listener {
+export class ReadyListener extends Listener<Events.ClientReady> {
   constructor(context: Listener.LoaderContext, options: Listener.Options) {
-    super(context, {
-      ...options,
-      once: true,
-      event: "ready",
-    });
+    super(context, { ...options, once: true, event: Events.ClientReady });
   }
 
-  override run(client: Client) {
-    container.logger.info("Logged in as:", client.user?.tag);
+  override run(...[client]: ClientEvents[Events.ClientReady]) {
+    container.logger.info("Logged in as:", client.user.tag);
   }
 }
