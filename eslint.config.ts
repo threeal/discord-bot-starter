60f3f6f56db71a4664738fd847f8501e0c8bf1b9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-23 12:26:02 +0700

    test: convert ESLint configuration to TypeScript
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- eslint.config.ts
+++ eslint.config.ts
@@ -8,9 +8,7 @@ export default tseslint.config(
   {
     languageOptions: {
       parserOptions: {
-        projectService: {
-          allowDefaultProject: ["eslint.config.js"],
-        },
+        projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
